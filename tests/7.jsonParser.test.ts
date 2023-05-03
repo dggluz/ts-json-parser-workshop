bf4388351c6f8cab4ebc1d99fdac8d18f4ca4e81
Gonzalo Gluzman <gonzalo.gluzman@naranjax.com>
2023-05-03 01:06:11 -0300

    test: 7. jsonParser (null & boolean)

---
+++ tests/7.jsonParser.test.ts
@@ -0,0 +1,51 @@
+import { jsonParser } from '../src';
+
+describe.skip('jsonParser (null & boolean)', () => {
+  it('Parses "null"', async () => {
+    // GIVEN:
+    // The "null" input
+    const input = 'null';
+
+    // WHEN:
+    // Parsing the input with the jsonParser
+    const { result, remaining } = await jsonParser(input);
+
+    // THEN:
+    // The result is the null value
+    expect(result).toBe(null);
+    // ...and the remaining is the empty string
+    expect(remaining).toBe('');
+  });
+
+  it('Parses "true"', async () => {
+    // GIVEN:
+    // The "true" input
+    const input = 'true';
+
+    // WHEN:
+    // Parsing the input with the jsonParser
+    const { result, remaining } = await jsonParser(input);
+
+    // THEN:
+    // The result is the true value
+    expect(result).toBe(true);
+    // ...and the remaining is the empty string
+    expect(remaining).toBe('');
+  });
+
+  it('Parses "false"', async () => {
+    // GIVEN:
+    // The "false" input
+    const input = 'false';
+
+    // WHEN:
+    // Parsing the input with the jsonParser
+    const { result, remaining } = await jsonParser(input);
+
+    // THEN:
+    // The result is the false value
+    expect(result).toBe(false);
+    // ...and the remaining is the empty string
+    expect(remaining).toBe('');
+  });
+});
