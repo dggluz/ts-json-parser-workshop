55cf194154c03dd3a4c4f47b6c8a15f01edba54e
Gonzalo Gluzman <gonzalo.gluzman@naranjax.com>
2023-05-06 19:22:23 -0300

    testing: 12. keepLeft

--- tests/12.keepLeft.test.ts
+++ tests/12.keepLeft.test.ts
@@ -1,38 +1,38 @@
-import { concat, fromStr } from '../src';
+import { fromStr, keepLeft } from '../src';
 
-describe.skip('"concat" parser', () => {
-  it('"fooBarParser" parses "foo"', async () => {
+describe.skip('keepLeft', () => {
+  it('"keepFoo" parses "foobar"', async () => {
     // GIVEN:
-    // A "fooBar" parser,
-    const fooBarParser = concat(fromStr('foo'), fromStr('bar'));
+    // A "keepFoo" parser
+    const keepFooParser = keepLeft(fromStr('foo'), fromStr('bar'));
     // ...and the "foobar" input
     const input = 'foobar';
 
     // WHEN:
-    // Parsing the input with the fooBarParser
-    const { result, remaining } = await fooBarParser(input);
+    // Parsing the input with the keepFooParser
+    const { result, remaining } = await keepFooParser(input);
 
     // THEN:
-    // The result is the "foobar" string
-    expect(result).toBe('foobar');
+    // The result is the "foo" string
+    expect(result).toBe('foo');
     // ...and the remaining is the empty string
     expect(remaining).toBe('');
   });
 
   it('Parses "foobar" with extra text', async () => {
     // GIVEN:
-    // A "fooBar" parser,
-    const fooBarParser = concat(fromStr('foo'), fromStr('bar'));
+    // A "keepFoo" parser
+    const keepFooParser = keepLeft(fromStr('foo'), fromStr('bar'));
     // ...and the "foobar" input, with extra text ("ABC")
     const input = 'foobarABC';
 
     // WHEN:
-    // Parsing the input with the fooBarParser
-    const { result, remaining } = await fooBarParser(input);
+    // Parsing the input with the keepFooParser
+    const { result, remaining } = await keepFooParser(input);
 
     // THEN:
-    // The result is the "foobar" string
-    expect(result).toBe('foobar');
+    // The result is the "foo" string
+    expect(result).toBe('foo');
     // ...and the remaining is the "ABC" string
     expect(remaining).toBe('ABC');
   });
@@ -41,14 +41,14 @@ describe.skip('"concat" parser', () => {
     expect.assertions(1);
     try {
       // GIVEN:
-      // A "fooBar" parser,
-      const fooBarParser = concat(fromStr('foo'), fromStr('bar'));
+      // A "keepFoo" parser
+      const keepFooParser = keepLeft(fromStr('foo'), fromStr('bar'));
       // ...and the empty string input
       const input = '';
   
       // WHEN:
-      // Parsing the input with the fooBarParser
-      await fooBarParser(input);
+      // Parsing the input with the keepFooParser
+      await keepFooParser(input);
     } catch (e) {
       if (e instanceof Error) {
         expect(e.message)
@@ -63,14 +63,14 @@ describe.skip('"concat" parser', () => {
     expect.assertions(1);
     try {
       // GIVEN:
-      // A "fooBar" parser,
-      const fooBarParser = concat(fromStr('foo'), fromStr('bar'));
+      // A "keepFoo" parser
+      const keepFooParser = keepLeft(fromStr('foo'), fromStr('bar'));
       // ...and the "baz" string input
       const input = 'baz';
   
       // WHEN:
-      // Parsing the input with the fooBarParser
-      await fooBarParser(input);
+      // Parsing the input with the keepFooParser
+      await keepFooParser(input);
     } catch (e) {
       if (e instanceof Error) {
         expect(e.message)
@@ -85,14 +85,14 @@ describe.skip('"concat" parser', () => {
     expect.assertions(1);
     try {
       // GIVEN:
-      // A "fooBar" parser,
-      const fooBarParser = concat(fromStr('foo'), fromStr('bar'));
+      // A "keepFoo" parser
+      const keepFooParser = keepLeft(fromStr('foo'), fromStr('bar'));
       // ...and a long string input
       const input = 'loooooong';
 
       // WHEN:
-      // Parsing the input with the fooBarParser
-      await fooBarParser(input);
+      // Parsing the input with the keepFooParser
+      await keepFooParser(input);
     } catch (e) {
       if (e instanceof Error) {
         expect(e.message)
