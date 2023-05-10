804b910d76a934dd55e7cbf1f629b933d0619db4
Gonzalo Gluzman <gonzalo.gluzman@naranjax.com>
2023-05-10 01:41:37 -0300

    testing: 26. spaceParser

---
+++ tests/26.spaceParser.test.ts
@@ -0,0 +1,99 @@
+import { spaceParser } from '../src';
+
+describe.skip('Space parser', () => {
+  it('Parses the empty string', async () => {
+    // GIVEN:
+    // The empty string input
+    const input = '';
+
+    // WHEN:
+    // Parsing the input with the spaceParser
+    const { result, remaining } = await spaceParser(input);
+
+    // THEN:
+    // The result is the empty string
+    expect(result).toBe('');
+    // ...and the remaining is the empty string
+    expect(remaining).toBe('');
+  });
+
+  it('Parses a single space string', async () => {
+    // GIVEN:
+    // A simple space string input
+    const input = ' ';
+
+    // WHEN:
+    // Parsing the input with the spaceParser
+    const { result, remaining } = await spaceParser(input);
+
+    // THEN:
+    // The result is the single space string
+    expect(result).toBe(' ');
+    // ...and the remaining is the empty string
+    expect(remaining).toBe('');
+  });
+
+  it('Parses multiple spaces', async () => {
+    // GIVEN:
+    // A multiple spaces string input
+    const input = '     ';
+
+    // WHEN:
+    // Parsing the input with the spaceParser
+    const { result, remaining } = await spaceParser(input);
+
+    // THEN:
+    // The result is the multiple spaces string
+    expect(result).toBe('     ');
+    // ...and the remaining is the empty string
+    expect(remaining).toBe('');
+  });
+
+  it('Parses the empty string with extra text', async () => {
+    // GIVEN:
+    // The empty string input, with extra text ("ABC")
+    const input = 'ABC';
+
+    // WHEN:
+    // Parsing the input with the spaceParser
+    const { result, remaining } = await spaceParser(input);
+
+    // THEN:
+    // The result is the empty string
+    expect(result).toBe('');
+    // ...and the remaining is the "ABC" string
+    expect(remaining).toBe('ABC');
+  });
+
+  it('Parses the single space string with extra text', async () => {
+    // GIVEN:
+    // The single space string input, with extra text ("ABC")
+    const input = ' ABC';
+
+    // WHEN:
+    // Parsing the input with the spaceParser
+    const { result, remaining } = await spaceParser(input);
+
+    // THEN:
+    // The result is the single space string
+    expect(result).toBe(' ');
+    // ...and the remaining is the "ABC" string
+    expect(remaining).toBe('ABC');
+  });
+
+  it('Parses the multiple spaces string with extra text', async () => {
+    // GIVEN:
+    // The multiple spaces string input, with extra text ("ABC")
+    const input = '     ABC';
+
+    // WHEN:
+    // Parsing the input with the spaceParser
+    const { result, remaining } = await spaceParser(input);
+
+    // THEN:
+    // The result is the multiple spaces string
+    expect(result).toBe('     ');
+    // ...and the remaining is the "ABC" string
+    expect(remaining).toBe('ABC');
+  });
+});
